d57dac6c1b89039acb76ae74c73f18f9801f3062
Felipe Avelar <felipe.avelar@checkmarx.com>
2021-08-06 17:50:35 +0100

    Codacy issues
    Signed-off-by: Felipe Avelar <felipe.avelar@checkmarx.com>

--- internal/console/kics.go
+++ internal/console/kics.go
@@ -99,10 +99,7 @@ func initialize(rootCmd *cobra.Command) error {
 		return err
 	}
 
-	if err := initScanCmd(scanCmd); err != nil {
-		return err
-	}
-	return nil
+	return initScanCmd(scanCmd)
 }
 
 // Execute starts kics execution
